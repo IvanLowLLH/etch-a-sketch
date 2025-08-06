d425822824e78149df4f0a82cbb5abf51ea36603
ivan <ivanlowlh@gmail.com>
2025-08-06 16:25:13 +0800

    Add button to change grid size and add logic

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let num_row = 16
-let num_col = 16
-let num_squares = num_row * num_col
+let grid_size = 16
 
 function createGrid () {
     const main_div_container = document.querySelector("#main-container")
-    for (let row_idx = 0; row_idx < num_row; row_idx++) {
+    for (let row_idx = 0; row_idx < grid_size; row_idx++) {
         const row_div = document.createElement("div")
         row_div.setAttribute("class", "row-div")
         main_div_container.appendChild(row_div)
-        for (let col_idx = 0; col_idx < num_col; col_idx++) {
+        for (let col_idx = 0; col_idx < grid_size; col_idx++) {
             const col_div = document.createElement("div")
             col_div.setAttribute("class", "sq-div")
             col_div.setAttribute("id", `${row_idx}-${col_idx}-sq`)
@@ -25,3 +23,20 @@ const sq_divs = document.querySelectorAll(".sq-div")
 sq_divs.forEach((sq_div) => {
             sq_div.addEventListener("mouseover", () => colorSquare(sq_div))
         })
+
+function setNewGridSize () {
+    let new_grid_size = prompt("Please enter the desired grid size (maximum 100): ", 16)
+    if (new_grid_size > 100) {
+        alert("Desired grid size too large. Please reduce to less than 100")
+        return
+    }
+    grid_size = new_grid_size
+    // Remove grid
+    const elementsToRemove = document.querySelectorAll('.row-div');
+    elementsToRemove.forEach(element => {
+        element.remove();
+    });
+    createGrid()
+}
+const grid_btn = document.querySelector("#set-grid")
+grid_btn.addEventListener("click", () => setNewGridSize())
